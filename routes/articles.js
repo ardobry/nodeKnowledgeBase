e8c99719a55b788a998205b9445d9cceda5ee2fa
Peace4Every1 <paligyan@gmail.com>
2019-04-30 20:25:47 +0400

    Added access control

--- routes/articles.js
+++ routes/articles.js
@@ -2,17 +2,36 @@ const express = require("express");
 const router = express.Router();
 //Bringing in the article model
 const Article = require('../models/article');
+//User Model
+const User = require('../models/user');
+
+
+//Access control
+const ensureAuthenticated = (req, res, next) => {
+    //req.isAuthenticated used because passport is available
+    if(req.isAuthenticated()){
+        return next();
+    } else {
+        req.flash('danger', 'Please login');
+        res.redirect('/users/login');
+    }
+}
+
 
 //Add route
-router.get("/add", (req, res) => {
+router.get("/add", ensureAuthenticated, (req, res) => {
     res.render("add_article", {
         title: "Add Article"
     });
 });
 
 //Load Edit Form
-router.get("/edit/:id", (req, res) => {
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
     Article.findById(req.params.id, (err, article) => {
+        if(article.author != req.user._id){
+            req.flash('danger', 'Not Authorized');
+           return res.redirect('/');
+        }
         res.render("edit_article", {
             title: "Edit Article",
             article
@@ -24,7 +43,7 @@ router.get("/edit/:id", (req, res) => {
 router.post("/add", (req, res) => {
     //Validator implementation
     req.checkBody("title", "Title is required").notEmpty();
-    req.checkBody("author", "Author is required").notEmpty();
+    // req.checkBody("author", "Author is required").notEmpty();
     req.checkBody("body", "Body is required").notEmpty();
 
     //Get Errors
@@ -39,7 +58,7 @@ router.post("/add", (req, res) => {
         //Need body parser for this to work
         let article = new Article();
         article.title = req.body.title;
-        article.author = req.body.author;
+        article.author = req.user._id;
         article.body = req.body.body;
 
         article.save(err => {
@@ -76,21 +95,34 @@ router.post("/edit/:id", (req, res) => {
 
 //Deleting an article
 router.delete("/:id", (req, res) => {
-  let query = { _id: req.params.id };
+    if(!req.user._id){
+        res.status(500).send();
+    }
 
-  Article.remove(query, err => {
-    if (err) console.log(err);
-    res.send("Success");
-  });
+    let query = { _id: req.params.id };
+
+    Article.findById(req.params.id, (err, article) => {
+        if(article.author != req.user._id){
+            res.status(500).send();
+        } else {
+            Article.remove(query, err => {
+                if (err) console.log(err);
+                res.send("Success");
+            });
+        }
+    });
 });
 
 
 //Get Single Article
 // : means that what's coming after is a placeholder and can be anything
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res) => {    
     Article.findById(req.params.id, (err, article) => {
-        res.render("article", {
-            article
+        User.findById(article.author, (err, user) => {
+            res.render("article", {
+                article,
+                author: user.name
+            });
         });
     });
 });
